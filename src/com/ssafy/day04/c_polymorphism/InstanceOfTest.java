bda57bc81b457cf38f8fd09e7971e660b7d8d0a1
minhe8564 <minhe8564@gmail.com>
2025-01-23 14:23:45 +0900

    update : day04

--- src/com/ssafy/day04/c_polymorphism/InstanceOfTest.java
+++ src/com/ssafy/day04/c_polymorphism/InstanceOfTest.java
@@ -4,30 +4,30 @@ import com.ssafy.day03.a_inheritance.person.Person;
 import com.ssafy.day03.a_inheritance.person.SpiderMan;
 
 public class InstanceOfTest {
-    private void unsafeCasting() {
-        Object obj = 1;
-        String s = (String) obj;
-        System.out.println(s.length()); // ClassCastException 오류 Integer는 .length() 사용불가
-    }
+	private void unsafeCasting() {
+		Object obj = 1;
+		String s = (String) obj;
+		// ClassCastException 오류 Integer는 .length() 사용불가
+		System.out.println(s.length());
+	}
 
-    private void safeCasting() {
-        // TODO: 위 메서드가 안전하게 형 변환할 수 있도록 수정해보자.
-    	Object obj = 1;
-    	if(obj instanceof Integer) { // obj 너 Integer 야???
-    		Integer num = (Integer)obj;
-    		System.out.println(num.intValue());
-    	}
-    	else {
-    		System.out.println("무슨 타입? " + obj.getClass().getName());
-    	}
+	private void safeCasting() {
+		// TODO: 위 메서드가 안전하게 형 변환할 수 있도록 수정해보자.
+		Object obj = 1;
+		if (obj instanceof Integer) { // obj 너 Integer 야???
+			Integer num = (Integer) obj;
+			System.out.println(num.intValue());
+		} else {
+			System.out.println("무슨 타입? " + obj.getClass().getName());
+		}
 
-        // END
-    }
+		// END
+	}
 
-    public static void main(String[] args) {
-        InstanceOfTest instance = new InstanceOfTest();
-        instance.unsafeCasting();
-        instance.safeCasting();
-    }
+	public static void main(String[] args) {
+		InstanceOfTest instance = new InstanceOfTest();
+		instance.unsafeCasting();
+		instance.safeCasting();
+	}
 
 }
