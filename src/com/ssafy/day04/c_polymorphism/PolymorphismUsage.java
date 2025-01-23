f830490e08b0767eb1894f843de584c2cc6f4211
minhe8564 <minhe8564@gmail.com>
2025-01-23 14:19:40 +0900

    update : day04

--- src/com/ssafy/day04/c_polymorphism/PolymorphismUsage.java
+++ src/com/ssafy/day04/c_polymorphism/PolymorphismUsage.java
@@ -2,25 +2,23 @@ package com.ssafy.day04.c_polymorphism;
 
 import com.ssafy.day03.a_inheritance.person.Person;
 import com.ssafy.day03.a_inheritance.person.SpiderMan;
+//3일차 Person, SpiderMan 참고
 
 public class PolymorphismUsage {
     public void useObjectArray() {
         // TODO:Object []을 선언하고 다양한 객체를 저장하고 저장된 클래스 타입을 출력하세요.
-    	// Day 3 Person, SpiderMan 참고
-    	
-    	Object[] objs = new Object[4];
+    	Object [] objs = new Object[4];
     	objs[0] = new SpiderMan("a", false);
     	objs[1] = new Person("b");
     	objs[2] = objs;
-    	objs[3] = 1; // autoboxing
-    	
+    	objs[3] = 1; //autoboxing
     	for(Object obj : objs) {
     		System.out.println(obj + " : " + obj.getClass().getName());
     	}
         // END
     }
 
-    public void useObjectParam() {
+    public void useObjectParam() { //println은 Object로 받는다.
         System.out.println(1);
         System.out.println("Hello");
         System.out.println(new Person("피터파커"));
