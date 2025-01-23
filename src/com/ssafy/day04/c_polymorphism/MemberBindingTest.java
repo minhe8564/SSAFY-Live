7be5dbbb923255e12426da5bb954109403841fdf
minhe8564 <minhe8564@gmail.com>
2025-01-23 15:16:57 +0900

    update : day04 MemberBinding

--- src/com/ssafy/day04/c_polymorphism/MemberBindingTest.java
+++ src/com/ssafy/day04/c_polymorphism/MemberBindingTest.java
@@ -1,29 +1,41 @@
 package com.ssafy.day04.c_polymorphism;
 
 class SuperClass {
-    String x = "super";
+	String x = "super";
 
-    public void method() {
-        System.out.println("super class method");
-    }
+	static void a() {
+		System.out.println("super class static method");
+	}
+
+	public void method() {
+		System.out.println("super class method");
+	}
 }
 
 class SubClass extends SuperClass {
-    String x = "sub";
+	String x = "sub";
 
-    @Override
-    public void method() {
-        System.out.println("sub class method");
-    }
+	static void a() {
+		System.out.println("sub class static method");
+	}
+
+	@Override
+	public void method() {
+		System.out.println("sub class method");
+	}
 }
 
 public class MemberBindingTest {
 
-    public static void main(String[] args) {
-        SubClass subClass = new SubClass();
-        // TODO: 참조 객체의 레벨에 따라 어떤 멤버 변수 또는 메서드가 동작하는지 확인하시오.
-
-        // END
-    }
+	public static void main(String[] args) {
+//        SubClass subClass = new SubClass();
+		SuperClass superClass = new SuperClass();
+		// TODO: 참조 객체의 레벨에 따라 어떤 멤버 변수 또는 메서드가 동작하는지 확인하시오.
+		superClass.a(); 
+		// END
+	}
 
 }
+
+//super class static method
+
